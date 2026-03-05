337ab846acc5065cfeb6ab3d6ad8725a3ce638df
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-03-05 09:32:22 +0000

    j

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import serverless from "serverless-http";
 
 export default async function handler(req: any, res: any) {
-  const mod = await import("../server/index.js");
-  const app = mod.default || mod.app || mod;
+  const mod = await import("../server/index.cjs");
+  const app = mod.default || mod;
 
   const server = serverless(app);
   return server(req, res);
