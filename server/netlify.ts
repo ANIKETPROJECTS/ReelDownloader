8dc601ffa26669a62124f21e187cc4fd9746c29f
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-03-05 09:19:30 +0000

    s

--- server/netlify.ts
+++ server/netlify.ts
@@ -1,6 +1,6 @@
 import serverless from "serverless-http";
 // @ts-ignore
-import app from "./index.js";
+import app from "./index.ts";
 
 export const handler = serverless(app);
 export default app;
